48f7a5d89d22dddf5835198dc35e5f5a7fdccf55
David Godwin <davidgodwin@MacBook-Pro.frontierlocal.net>
2021-05-31 11:11:36 -0400

    Connect bookitem as a child component of BookList

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,26 +3,28 @@
         <h1>{{ title }}</h1>
 
         <ul>
-            <li v-for="book in books" :key="book.title">
-                {{ book.title }} : {{ book.author }}
-            </li>
+            <book-item v-for="book in books" :book="book" :key="book.title">
+            </book-item>
         </ul>
     </div>
 </template>
 
 <script>
+    import BookItem from "./BookItem";
+
     export default {
-        name: "BookList",
-        data() {
-            return {
-                title: "All Books",
-                books: [
-                    {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
-                    {title: 'American Gods', author: 'Neil Gaiman'},
-                    {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
-                ],
-            }
-        }
+        components: { BookItem },
+                name: "BookList",
+                data() {
+                    return {
+                        title: "All Books",
+                        books: [
+                            {title: 'Self-Reliance', author: 'Ralph Waldo Emerson'},
+                            {title: 'American Gods', author: 'Neil Gaiman'},
+                            {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
+                        ],
+                    }
+                }
     }
 </script>
 
